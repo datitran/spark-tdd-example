8042423bc3332affb9b8e2f9c5c72209782405a7
Dat Tran <datitran@gmail.com>
2017-01-03 14:01:00 +0100

    Create mock data through mock csv file than rdd.

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,15 +1,26 @@
 from . import basetests
-import clustering
+from pyspark.sql.types import *
+
+import os
+import clustering  # script file needs to be imported after basetests
 
 
 class ClusteringTest(basetests.BaseTestClass):
     def mock_data(self):
         """Mock data to imitate read from database."""
-        mock_data_rdd = self.spark_context.parallelize([("A", 1, 1), ("B", 1, 0),
-                                                        ("C", 0, 2), ("D", 2, 4),
-                                                        ("E", 3, 5)])
-        schema = ["id", "x", "y"]
-        mock_data_df = self.spark_session.createDataFrame(mock_data_rdd, schema)
+        schema = StructType([
+            StructField("id", StringType(), True),
+            StructField("x", DoubleType(), True),
+            StructField("y", DoubleType(), True),
+        ])
+
+        mock_data_df = (self.spark_session
+            .read
+            .format("csv")
+            .schema(schema)
+            .load(os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "mock_data.csv"))
+        )
         return mock_data_df
 
     def test_count(self):
@@ -42,4 +53,4 @@ class ClusteringTest(basetests.BaseTestClass):
         scaled_df = clustering.rescale_df(input_df)
         label_df = clustering.assign_cluster(scaled_df)
         self.assertEqual(label_df.rdd.map(lambda x: x.label).collect(),
-                         [0, 0, 0, 1, 1])
+                         [1, 1, 1, 0, 0])
